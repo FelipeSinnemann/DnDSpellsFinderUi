95686bf7e83753fc31e79b84b6b0fd4e2930c035
Felipe Sinnemann <felipe.a.malheiros.s@gmail.com>
2025-03-02 15:51:27 -0300

    First commit - Functional interface with API request

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,27 @@
 import { Routes } from '@angular/router';
+import { SpellsComponent } from './pages/spells/spells.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'spells',
+        component: SpellsComponent
+    },
+    {
+        path: '',
+        redirectTo: '/spells',
+        pathMatch: 'full'
+    },
+    {
+        path: 'home',
+        redirectTo: '/spells'
+    },
+    {
+        path: '404',
+        component: PageNotFoundComponent
+    },
+    {
+        path: '**',
+        redirectTo: '/404'
+    },
+];
