001d4026ff2a19f392891feb27ce5f3335995cf3
Felipe Sinnemann <felipe.a.malheiros.s@gmail.com>
2025-03-06 11:07:09 -0300

    Fix: Fixing Spell model attributes

--- src/app/models/spell/spell.ts
+++ src/app/models/spell/spell.ts
@@ -1,38 +1,53 @@
 export class Spell {
     id: number;
+    school_id: number;
     name: string;
     level: number;
-    school: string;
-    castingTime: string;
-    range: string;
-    components: string;
-    duration: string;
     description: string;
-    materials: string;
+    special_description: boolean;
+    range: string;
+    casting_time: string;
+    duration: string;
+    concentration: boolean;
+    ritual: boolean;
+    verbal: boolean;
+    somatic: boolean;
+    material: boolean;
+    material_components: string;
     showMaterials: boolean;
     showDescription: boolean;
     constructor(
         id: number,
+        school_id: number,
         name: string,
         level: number,
-        school: string,
-        castingTime: string,
-        range: string,
-        components: string,
-        duration: string,
         description: string,
-        materials: string,
+        special_description: boolean,
+        range: string,
+        casting_time: string,
+        duration: string,
+        concentration: boolean,
+        ritual: boolean,
+        verbal: boolean,
+        somatic: boolean,
+        material: boolean,
+        material_components: string,
     ) {
         this.id = id;
+        this.school_id = school_id;
         this.name = name;
         this.level = level;
-        this.school = school;
-        this.castingTime = castingTime;
-        this.range = range;
-        this.components = components;
-        this.duration = duration;
         this.description = description;
-        this.materials = materials;
+        this.special_description = special_description;
+        this.range = range;
+        this.casting_time = casting_time;
+        this.duration = duration;
+        this.concentration = concentration;
+        this.ritual = ritual;
+        this.verbal = verbal;
+        this.somatic = somatic;
+        this.material = material;
+        this.material_components = material_components;
         this.showMaterials = false;
         this.showDescription = false;
     }
