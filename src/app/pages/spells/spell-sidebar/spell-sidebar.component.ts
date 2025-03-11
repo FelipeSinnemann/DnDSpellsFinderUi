d1348c7ef803095b996d39332c6fc62e2f7254d7
Felipe Sinnemann <felipe.a.malheiros.s@gmail.com>
2025-03-11 11:58:39 -0300

    Feat: Using innerHtml to create tables and stuff inside spells description

--- src/app/pages/spells/spell-sidebar/spell-sidebar.component.ts
+++ src/app/pages/spells/spell-sidebar/spell-sidebar.component.ts
@@ -1,7 +1,8 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Spell } from '../../../models/spell/spell';
 import { NgIf } from '@angular/common';
 import { SchoolsEnum } from '../../../enums/schools';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-spell-sidebar',
@@ -9,13 +10,31 @@ import { SchoolsEnum } from '../../../enums/schools';
   templateUrl: './spell-sidebar.component.html',
   styleUrl: './spell-sidebar.component.scss'
 })
-export class SpellSidebarComponent {
-  @Input() spell: Spell | null = null;
+export class SpellSidebarComponent implements OnInit {
+  public spell: Spell | null;
+  @Input() set selectedSpell(spell: Spell | null){
+    this.spell = spell;
+    this.sanitizeDescription()
+  }
   @Output() closedSidebar: EventEmitter<any> = new EventEmitter();
 
   schoolsEnum = SchoolsEnum;
   
-  constructor() {
+  public sanitizedDescription: SafeHtml;
+
+  constructor(private sanitizer: DomSanitizer) {
+  }
+
+  ngOnInit(): void {
+    this.sanitizeDescription()
+  }
+
+  sanitizeDescription() {
+    if(!this.spell){
+      return;
+    }
+
+    this.sanitizedDescription = this.sanitizer.bypassSecurityTrustHtml(this.spell?.description);
   }
 
   public closeSidebar(){
